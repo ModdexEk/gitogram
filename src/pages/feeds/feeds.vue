4471d4c59fd1e4385d0237aa8a6e4e502bf6d74e
ModdexEk <moddex@yandex.ru>
2024-06-02 14:30:20 +0500

    Компоненты хедера и иконок

---
+++ src/pages/feeds/feeds.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="topline">
+        <topline>
+            <template #headline>
+                <div class="icon">
+                    <icon name="home" />
+                </div>
+            </template>
+            <template #content>
+                <h2>Stories</h2>
+            </template>
+        </topline>
+    </div>
+</template>
+
+<script>
+import { topline } from '../../components/topline'
+import { icon } from '../../icons'
+export default {
+  name: 'feeds',
+  components: {
+    topline,
+    icon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .icon {
+    color: red;
+    width: 50px;
+  }
+</style>
